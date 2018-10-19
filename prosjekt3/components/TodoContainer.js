b129358d30a2e60a5c7e00287691cb0003fd1e2a
Ingrid Gormsdatter Draagen <ingrid.draagen@gmail.com>
2018-10-19 16:09:48 +0200

    added checkboxes and components. #3

--- prosjekt3/components/TodoContainer.js
+++ prosjekt3/components/TodoContainer.js
@@ -1,18 +1,62 @@
-import React from 'react';
-import {
-  Text,
-  ScrollView,
- } from 'react-native';
+import React, { Component } from 'react';
+import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
+import { CheckBox } from 'react-native-elements'
+import TabBarIcon from './TabBarIcon';
+const check = 1;
 
- export default class TodoContainer extends React.Component {
-   render() {
-     return (
-       <ScrollView>
-       <Text>lålål</Text>
-       <Text>lålål</Text>
-       <Text>lålål</Text>
+class ScrollViewExample extends Component {
+   state = {
 
-       </ScrollView>
-     );
+      todos: [
+         {'task': 'Ben', 'id': 1, 'checked':0},
+         {'task': 'Susan', 'id': 2, 'checked':0},
+         {'task': 'Robert', 'id': 3, 'checked':0},
+         {'task': 'Mary', 'id': 4, 'checked':0},
+         {'task': 'Daniel', 'id': 5, 'checked':1},
+         {'task': 'Laura', 'id': 6, 'checked':0},
+         {'task': 'John', 'id': 7, 'checked':0},
+         {'task': 'Debra', 'id': 8, 'checked':0},
+         {'task': 'Aron', 'id': 9, 'checked':0},
+         {'task': 'Ann', 'id': 10, 'checked':0},
+         {'task': 'Steve', 'id': 11, 'checked':0},
+         {'task': 'Olivia', 'id': 12, 'checked':1}
+      ]
    }
- }
+   render() {
+      return (
+         <View>
+            <ScrollView>
+               {
+                  this.state.todos.map((item, index) => (
+                     <View key = {item.id} style = {styles.item} >
+                     check = {item.checked}
+                        <Text>{item.task}                                                                                                       </Text>
+                          <TabBarIcon
+                            name={
+                                 `md-checkbox${ item.checked ? '' : '-outline'}`
+                            }
+                          />
+                        ),
+                     </View>
+                  ))
+               }
+            </ScrollView>
+            <Text>  </Text>
+         </View>
+      )
+   }
+}
+export default ScrollViewExample
+
+const styles = StyleSheet.create ({
+   item: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+      padding: 30,
+      margin: 2,
+      borderColor: '#23B5D3',
+      borderWidth: 1,
+      backgroundColor: '#F7F7FF'
+   }
+})
